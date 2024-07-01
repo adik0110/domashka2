917d0d5e57bfa1b0e05370878fe8719dd9ec82c5
Adel Salyamov <adelsalyamov@gmail.com>
2024-07-01 23:17:42 +0300

    Initial commit

---
+++ app/src/main/java/ru/itis/summerpractice24/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.itis.summerpractice24
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
+}
+
